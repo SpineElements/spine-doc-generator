d613196538087783755c145a69b0b4001d6c31e2
Nikolay Galanov <galanov.nick@gmail.com>
2017-05-26 13:50:48 +0300

    Add dialog for doc file overwriting

--- src/index.js
+++ src/index.js
@@ -10,78 +10,87 @@ const {
 const escape = require('html-escape');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 module.exports = {
   generate: argv => {
     const templateFilePath = path.join(__dirname, "template", "element-doc-template.html");
-    const itemsRootPath = path.resolve(argv[2] || './');
+    const args = {
+      root: path.resolve("./"),
+      overwrite: argv[2] === "-f"
+    };
 
     const analyzer = new Analyzer({
-      urlLoader   : new FSUrlLoader(itemsRootPath),
+      urlLoader   : new FSUrlLoader(args.root),
       urlResolver : new PackageUrlResolver()
     });
 
     analyzer.analyzePackage().then(_package => {
       const metadata = generateAnalysis(_package, '');
 
-      const processingPackage = !Boolean(
-          metadata.elements.find(item => item.path.indexOf(path.sep) === -1));
+      console.log(`Processing element from the ${args.root} directory`);
 
-      const elements = splitByElements(metadata.elements, itemsRootPath);
-
-      console.log(`Processing ${processingPackage ? "package" : "element"} ` +
-                  `from the ${itemsRootPath} directory`);
-
-      createDocFiles(templateFilePath, elements, itemsRootPath, processingPackage);
+      createDocFile(templateFilePath, metadata.elements, args);
     });
   }
 };
 
-function splitByElements(rawElementsData, itemsRootPath) {
-  const elements = {};
-
-  rawElementsData.forEach(item => {
-    const elementRootPath =
-        item.path.substring(0, item.path.indexOf(path.sep)) || getElementName(itemsRootPath);
-
-    if (elements[elementRootPath]) {
-      elements[elementRootPath].push(item);
-    } else {
-      elements[elementRootPath] = [item];
-    }
-  });
-
-  return elements;
-}
-
-function createDocFiles(templateFilePath, elements, itemsRootPath, processingPackage) {
+function createDocFile(templateFilePath, elements, args) {
   fs.readFile(templateFilePath, 'utf-8', (err, data) => {
     if (err) {
       throw new Error(`Failed to read the template\n${err.message}`);
     }
 
-    Object.keys(elements).forEach((rootPath, index) => {
-      const docFilePath = processingPackage
-          ? `${itemsRootPath}/${rootPath}/index.html`
-          : `${itemsRootPath}/index.html`;
+    const docFilePath = `${args.root}${path.sep}index.html`;
+    const docFileContent = fillContent(data, elements, args.root);
 
-      const docFileContent = fillContent(data, elements[rootPath], rootPath);
-      fs.writeFile(docFilePath, docFileContent, err => {
-        if (err) {
-          throw new Error(`Failed to fill the doc file\n${err.message}`);
-        }
-        if (index === Object.keys(elements).length - 1) {
-          console.log('Documentation is generated!')
-        }
+    if (!args.overwrite && fs.existsSync(docFilePath)) {
+      const rlInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
       });
-    });
+
+      rlInterface.question(`File ${docFilePath} already exists.\nOverwrite it? |no| `, answer => {
+        fileExistsDialog(answer, rlInterface, () => writeDocFile(docFilePath, docFileContent));
+      });
+    } else {
+      writeDocFile(docFilePath, docFileContent);
+    }
   });
 }
 
-function fillContent(fileData, elements, elementRoot) {
-  const mainElement = elements.find(item => item.tagname === elementRoot);
+function fileExistsDialog(answer, rlInterface, successCallback) {
+  const lowerCaseAnswer = answer.toLowerCase();
+  if (lowerCaseAnswer === "yes" || lowerCaseAnswer === "y") {
+    successCallback();
+    rlInterface.close();
+
+  } else if (lowerCaseAnswer === "no" || lowerCaseAnswer === "n") {
+    console.log('Documentation generation canceled.');
+    process.exit(0);
+
+  } else {
+    console.log('Please enter "yes" or "no".\n');
+    rlInterface.question(`Overwrite it? |no| `, answer => {
+      fileExistsDialog(answer, rlInterface, successCallback);
+    });
+  }
+}
+
+function writeDocFile(filePath, content) {
+  fs.writeFile(filePath, content, err => {
+    if (err) {
+      throw new Error(`Failed to fill the doc file\n${err.message}`);
+    }
+    console.log(`Created file: ${filePath}\nDocumentation generation completed.`);
+  });
+}
+
+function fillContent(fileData, elements, itemsRootPath) {
+  const mainElementName = getElementName(itemsRootPath);
+  const mainElement = elements.find(item => item.tagname === mainElementName);
   const sortedElements = elements.length > 1 && mainElement
-      ? [mainElement, ...elements.filter(item => item.tagname !== elementRoot)]
+      ? [mainElement, ...elements.filter(item => item.tagname !== mainElementName)]
       : elements;
 
   const elementNavigationItems = sortedElements.length > 1
@@ -93,10 +102,10 @@ function fillContent(fileData, elements, elementRoot) {
       : getElementDocViewer(sortedElements[0]);
 
   return fileData
-    .replace('__TITLE__', elementRoot)
+    .replace('__TITLE__', mainElementName)
     .replace('__ELEMENT_NAVIGATION_ITEMS__', elementNavigationItems)
     .replace('__ELEMENTS_CONTENT__', elementsContent)
-    .replace('__MAIN_ELEMENT__', elementRoot);
+    .replace('__MAIN_ELEMENT__', mainElementName);
 }
 
 function getElementDocViewer(rawElement) {
